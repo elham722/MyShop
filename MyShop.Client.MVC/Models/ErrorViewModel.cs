508e999bcf794952ae847e0c88efbeaf7a5c55ce
elham722 <elham.heydari722@gmail.com>
2025-09-13 09:37:41 +0330

    Add project files.

---
+++ MyShop.Client.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyShop.Client.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
