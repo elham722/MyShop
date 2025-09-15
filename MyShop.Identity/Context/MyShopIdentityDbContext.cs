bfacb30505f01145b8021dbba6dfa80d3a1e7897
elham722 <elham.heydari722@gmail.com>
2025-09-15 11:15:54 +0330

    add dbcontext for identity and registration service

---
+++ MyShop.Identity/Context/MyShopIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using MyShop.Identity.Models;
+
+namespace MyShop.Identity.Context
+{
+    public class MyShopIdentityDbContext:IdentityDbContext<ApplicationUser>
+    {
+    }
+}
---
+++ MyShop.Identity/DependencyInjection/IdentityServicesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyShop.Identity.Context;
+
+namespace MyShop.Identity.DependencyInjection
+{
+    public static class IdentityServicesRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<MyShopIdentityDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("IdentityDBConnection")));
+
+            return services;
+        }
+
+    }
+}
