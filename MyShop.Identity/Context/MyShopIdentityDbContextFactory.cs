dd5d30227fbe17e1aca9ac5e00decfbc9067d2e8
elham722 <elham.heydari722@gmail.com>
2025-09-20 08:52:18 +0330

    migration not

---
+++ MyShop.Identity/Context/MyShopIdentityDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MyShop.Identity.Context;
+
+namespace MyShop.Identity.Context
+{
+    public class MyShopIdentityDbContextFactory : IDesignTimeDbContextFactory<MyShopIdentityDbContext>
+    {
+        public MyShopIdentityDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MyShopIdentityDbContext>();
+
+            // مستقیم Connection String داخل Factory
+            optionsBuilder.UseSqlServer(
+                "Data Source=.;Initial catalog=BackendIdentity_DB;Integrated security=true;TrustServerCertificate=True;");
+
+            return new MyShopIdentityDbContext(optionsBuilder.Options);
+        }
+    }
+}
